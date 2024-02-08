e9e14863d56d1c9f25ee76b62796e4453685e154
maithani74 <maithanir74@gmail.com>
2024-02-08 08:49:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose")
+const express = require("express")
+const cors = require("cors")
+const morgan  = require("morgan");
+const app = express();
+const taskRouter = require('./routes/todoRoutes')
+const path = require('path')
+const dotenv = require("dotenv")
+
+app.use(express.static(path.join(__dirname,"./todo-app/build")))
+app.use(morgan("dev"));
+app.use(cors())
+app.use(express.json())
+
+dotenv.config()
+
+const connectdb = async()=>{
+    try{
+        const cons  =await mongoose.connect(process.env.mongoURL)
+        console.log("Connected Successfully")
+    }catch(err){
+        console.log(err)
+    }
+}
+
+connectdb();
+app.use("/",taskRouter.router)
+app.use("*",function(req,res){
+    res.sendFile(path.join(__dirname,"./todo-app/build/index.html"))
+})
+app.listen(process.env.port,()=>{
+    console.log(`Server Started Listening on port ${process.env.port}`)
+})
